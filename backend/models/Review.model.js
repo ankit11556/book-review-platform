e68a7702dd2edb6796bfb1114cee23c685f11b89
ankit <saharan442090@gmail.com>
2025-10-05 01:15:55 +0530

    feat(review): create review schema

---
+++ backend/models/Review.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  bookId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Book",
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+   reviewText: {
+    type: String,
+    trim: true,
+    },
+},{timestamps: true})
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review
