38837df9676870ffd356653b21b955e7be6e9175
ankit <saharan442090@gmail.com>
2025-10-04 18:23:19 +0530

    feat(auth): add auth middleware

---
+++ backend/middlewares/Auth.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const protect = async (req,res,next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token,process.env.TOKEN_KEY)
+
+      req.user = await User.findById(decoded.userId).select("-password")
+
+      next()
+    } catch (error) {
+      res.status(401).json({ message: "Invalid or expired token" });
+    }
+  } else{
+    res.status(401).json({ message: "No token, authorization denied" });
+  }
+}
+
+module.exports = protect
