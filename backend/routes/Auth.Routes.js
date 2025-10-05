ef46073290ea0dbd9b36f5fbefde35c50e86c71c
ankit <saharan442090@gmail.com>
2025-10-05 15:26:09 +0530

    feat: add check auth route

--- backend/routes/Auth.Routes.js
+++ backend/routes/Auth.Routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {signupController,loginController} = require('../controllers/Auth.controller')
+const {protect} = require("../middlewares/Auth.middleware")
 
 router.post("/sign-up", signupController)
 router.post("/login", loginController)
+
+router.get("/check-auth",protect,(req,res)=>{
+  res.json({user: req.user})
+})
 
 module.exports = router
